fb9b39606c7bffc154c326c6f81be7ac49bd940e
x-code-x-sudo <k.xusan_2003@mail.ru>
2023-08-08 16:59:29 +0500

    noisereduce with tts added

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from volume import increase_vol, decrease_vol
 
 from gtts import gTTS
 
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(11, GPIO.OUT)
+# GPIO.output(11, 1)
+
 # Function for text-to-speech
 def speak(text):
     tts = gTTS(text=text, lang='en')
@@ -30,18 +34,70 @@ def recognize_speech():
         text = recognizer.recognize_google(audio)
         return text
     except sr.UnknownValueError:
-        print("Could not understand the audio.")
+        print("Sorry, I couldn't understand that.")
     except sr.RequestError as e:
         print(f"Error accessing the Google Web Speech API: {e}")
 
     return None
+
+
+def listen():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source, timeout=5)
+        print(audio)
+
+    try:
+        print("Recognizing...")
+        text = recognizer.recognize_google(audio)
+        return text
+    except sr.UnknownValueError:
+        print("Sorry, I couldn't understand that.")
+    except sr.RequestError as e:
+        print(f"Error accessing the Google Web Speech API: {e}")
+
+    return None
+
+def main():
+    sample_rate = 44100
+    duration = 5
+
+    print("Recording audio...")
+
+    # Recording audio from the microphone
+    recording = sd.rec(int(sample_rate * duration), samplerate=sample_rate, channels=1)
+    sd.wait()
+
+    print("Audio recorded. Processing...")
+
+    # Applying noise reduction algorithm
+    denoised_audio = nr.reduce_noise(y=recording.flatten(), sr=sample_rate)
+
+    # Creating a Recognizer object
+    recognizer = sr.Recognizer()
+
+    # Using denoised audio for speech recognition
+    with sr.AudioData(denoised_audio.tobytes(), sample_rate, 2) as source:
+        print("Speak something:")
+        audio = recognizer.listen(source)
+
+    # Trying to recognize the voice command
+    try:
+        print("You said: " + recognizer.recognize_google(audio, language="en-US"))
+    except sr.UnknownValueError:
+        print("Could not understand the audio.")
+    except sr.RequestError as e:
+        print("Error accessing the Google Web Speech API: {0}".format(e))
 
 if __name__ == "__main__":
     remember_mode = False
 
     while True:
         try:
-            result = recognize_speech()
+            result = listen()
 
             if result:
                 print("You said:")
